4067e51a4b843968c3bcb3ebd7d76fd16d87fb82
Ananya Devpura <adevpura@ece001.ece.local.cmu.edu>
2026-01-16 09:35:27 -0500

    updated control loop but not tested

--- ControlLoop.sv
+++ ControlLoop.sv
@@ -39,27 +39,56 @@
   *
   * */
 module ControlLoop(
-    input  logic       clock, reset, start,
-    input  logic [8:0] mpu_x, mpu_y, mpu_z, // These are all in degrees
-    input  logic [8:0] set_x, set_y, set_z,
-  	output logic [9:0] out_x, out_y, out_z
+    input  logic       clock, reset, start, 
+	input  logic signed [8:0] pitch_kP, pitch_kI, pitch_kD, //from bluetooth module for testing 
+    input  logic signed [8:0] yaw_kP, yaw_kI, yaw_kD, //from bluetooth module for testing 
+	input  logic signed [8:0] mpu_pitch, mpu_yaw,  //from mpu controller
+	input  logic signed [8:0] set_pitch, set_yaw, //from bluetooth
+  	output logic signed [9:0] target_speed_left, target_speed_right
 );
 
-  logic [8:0] kP, kI, kD; // Scaling constants
+  //logic [8:0] kP, kI, kD; // Scaling constants
   logic [9:0] P, I, D; // summed values
 
+  logic starting; //Don't know if we need this 
 
-  logic starting;
+	logic signed [9:0] pitch_correction;
+    logic signed [9:0] yaw_correction;
 
-	PID #(.KP(1), .KI(1), .KD(1), .WIDTH(9)) xPID(.set_point(set_x),
-																				.process_point(mpu_x),
-																				.out(out_x), .*);
-	PID #(.KP(1), .KI(1), .KD(1), .WIDTH(1)) yPID(.set_point(set_y),
-																				.process_point(mpu_y),
-																				.out(out_y), .*);
-	PID #(.KP(1), .KI(1), .KD(1), .WIDTH(1)) zPID(.set_point(set_z),
-																				.process_point(mpu_z),
-																				.out(out_z), .*);
+	PID pitch_pid(.clock(clock), 
+				  .reset(reset), 
+				  .set_point(set_pitch), 
+				  .process_point(mpu_pitch), 
+				  .out(pitch_correction), 
+				  .KP(pitch_kP), 
+				  .KI(pitch_kI),
+				  .KD(pitch_kD));
+
+	PID yaw_pid(.clock(clock), 
+				.reset(reset), 
+				.set_point(set_yaw), 
+				.process_point(mpu_yaw), 
+				.out(yaw_correction), 
+				.KP(yaw_kP),  
+				.KI(yaw_kI),
+				.KD(yaw_kD));
+
+	logic signed [11:0] full_left, full_right;
+	// Mixing Logic
+    always_comb begin
+        // The mixing logic turns the two PID corrections into individual motor speeds.
+        full_left  = pitch_correction + yaw_correction;
+        full_right = pitch_correction - yaw_correction;
+
+		//in case bigger than allowed 
+		if (full_left > 10'sd511)  target_speed_left = 10'sd511;
+    	else if (full_left < -10'sd511) target_speed_left = -10'sd511;
+    	else target_speed_left = full_left[9:0];
+
+		if (full_right > 10'sd511)  target_speed_right = 10'sd511;
+    	else if (full_right < -10'sd511) target_speed_right = -10'sd511;
+    	else target_speed_right = full_right[9:0];
+    end
 
 	always_ff @(posedge clock) begin
 		if(reset) begin
@@ -75,57 +104,36 @@ endmodule: ControlLoop
 // KP, KI, KD are all constants that are specific each PID
 // The WIDTH controls how large each 'variable' needs to be
 module PID
-#(parameter KP = 1, KI = 1, KD = 1, WIDTH = 9)
+#(parameter WIDTH = 9)
 (
+	input  logic signed [8:0] KP, KI, KD,
 	input  logic clock, reset,
-	input  logic [8:0] set_point, process_point,
-	output logic [9:0] out
+	input  logic signed [8:0] set_point, process_point,
+	output logic signed [9:0] out
 );
 
 logic [WIDTH:0] error, prev_error;
-logic [WIDTH:0] P, I, D;
+logic signed [31:0] P, I, D;
+logic signed [31:0] integral;
 
 assign error = set_point - process_point;
 
-assign out = P + I + D;
-
 always_ff @(posedge clock, posedge reset) begin
-	if(reset) begin
-		prev_error <= '0;
-	end
-	else begin
-		prev_error <= error;
-	end
+        if (reset) begin
+            prev_error <= 0;
+            integral   <= 0;
+        end else begin
+            prev_error <= error;
+            // saturation to prevent windup
+            if (integral < 32'sd100000 && integral > -32'sd100000) 
+                integral <= integral + error;
+        end
 end
 
-// Proportional error
-always_ff @(posedge clock, posedge reset) begin
-	if(reset) begin
-		P <= '0;
-	end
-	else begin
-		P <= KP * error;
-	end
-end
+assign P = error * KP;
+assign I = integral * KI;
+assign D = (error - prev_error) * KD;
 
-// Integral error
-always_ff @(posedge clock, posedge reset) begin
-	if(reset) begin
-		I <= '0;
-	end
-	else begin
-		I <= KI * (I + error);
-	end
-end
-
-// Derivative error
-always_ff @(posedge clock, posedge reset) begin
-	if(reset) begin
-		D <= '0;
-	end
-	else begin
-		D <= KD * (error - prev_error);
-	end
-end
+assign out = (P + I + D) >>> 8;
 
 endmodule: PID
